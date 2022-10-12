351a968a77ea5ef2b237b41107ed5dc06306bba6
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-10-12 22:59:08 +0200

    Validate the webhook signature
    Thanks to a secret stored with the GitHub App, there is a relatively
    easy way to validate that GitHub sent the payload.
    
    Let's do that, and refuse to do _anything_ if there is no signature or
    the signature is invalid.
    
    For this to work, the `GITHUB_WEBHOOK_SECRET` variable has to be set in
    the Azure Function's `Application Settings` tab of the `Configuration`
    section.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- LeaveMeAloneGitHubApp/index.js
+++ LeaveMeAloneGitHubApp/index.js
@@ -1,4 +1,41 @@
+const crypto = require('crypto')
+
+/** Validates the signature added by GitHub */
+const validateGitHubWebHook = (context) => {
+    const secret = process.env['GITHUB_WEBHOOK_SECRET']
+    if (!secret) {
+        throw new Error('Webhook secret not configured')
+    }
+    if (context.req.headers['content-type'] !== 'application/json') {
+        throw new Error(`Unexpected content type: ${context.req.headers['content-type']}`)
+    }
+    const signature = context.req.headers['x-hub-signature-256']
+    if (!signature) {
+        throw new Error('Missing X-Hub-Signature')
+    }
+    const sha256 = signature.match(/^sha256=(.*)/)
+    if (!sha256) {
+        throw new Error(`Unexpected X-Hub-Signature format: ${signature}`)
+    }
+    const computed = crypto.createHmac('sha256', secret).update(context.req.rawBody).digest('hex')
+    if (sha256[1] !== computed) {
+        throw new Error('Incorrect X-Hub-Signature')
+    }
+}
+
 module.exports = async function (context, req) {
+    try {
+        validateGitHubWebHook(context)
+    } catch (e) {
+        context.log(e)
+        context.res = {
+            status: 403,
+            body: `Go away, you are not a valid GitHub webhook: ${e}`,
+        }
+        context.done()
+        return
+    }
+
     context.log('JavaScript HTTP trigger function processed a request.')
 
     const name = (req.query.name || (req.body && req.body.name))
