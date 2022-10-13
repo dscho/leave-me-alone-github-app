7fc0c4d5f7dbc9cb8f42ff4535460cdb2193132d
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-10-13 03:38:02 +0200

    Delete installation as soon as a new one is reported
    To do this, we need to use the app authentication, which in turn
    requires the private key of the GitHub App. The private key has an
    idiomatic, multi-line format, and won't work as a single line. However,
    the only way to store this in a Function App is as an environment
    variable, i.e. single line. To help with this, we store the inner part
    (which can lose its newline characters and still work) under the key
    `GITHUB_APP_PRIVATE_KEY` in the `Application settings` tab of the
    `Configuration` section in the Function App blade.
    
    The strategy implemented in this commit uses a hand-rolled JWT-signing
    function because we do not need to pull in ~600kB worth of Javascript
    _just_ for the `jsonwebtoken` package. The JWT signing is actually
    pretty trivial, once you know how it works.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- LeaveMeAloneGitHubApp/index.js
+++ LeaveMeAloneGitHubApp/index.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto')
+const https = require('https')
 
 /** Validates the signature added by GitHub */
 const validateGitHubWebHook = (context) => {
@@ -23,6 +24,80 @@ const validateGitHubWebHook = (context) => {
     }
 }
 
+/** Sends a JWT-authenticated GitHub API request */
+const sendAuthenticatedGitHubAPIRequest = (context, appId, requestMethod, requestPath, body) => {
+    const header = {
+        "alg": "RS256",
+        "typ": "JWT"
+    }
+
+    const now = Math.floor(new Date().getTime() / 1000)
+
+    const payload = {
+        // issued at time, 60 seconds in the past to allow for clock drift
+        iat: now - 60,
+        // JWT expiration time (10 minute maximum)
+        exp: now + (10 * 60),
+        // GitHub App's identifier
+        iss: appId
+    }
+
+    const toBase64 = (obj) => Buffer.from(JSON.stringify(obj), "utf-8").toString("base64url")
+	const headerAndPayload = `${toBase64(header)}.${toBase64(payload)}`
+
+    const privateKey = `-----BEGIN RSA PRIVATE KEY-----\n${process.env['GITHUB_APP_PRIVATE_KEY']}\n-----END RSA PRIVATE KEY-----\n`
+
+    const signer = crypto.createSign("RSA-SHA256")
+    signer.update(headerAndPayload)
+    const signature = signer.sign({
+        key: privateKey
+    }, "base64url")
+
+    const token = `${headerAndPayload}.${signature}`
+
+    return new Promise((resolve, reject) => {
+      const request = https.request({
+            host: "api.github.com",
+            port: 443,
+            path: requestPath,
+            method: requestMethod || "GET",
+            headers: {
+                "User-Agent": "curl/7.68.0",
+                Authorization: `Bearer ${token}`,
+                Accept: "application/vnd.github+json"
+            }
+        }, (res, e) => {
+            if (e) {
+                reject(e)
+                return
+            }
+            context.log(`${requestPath} returned ${res.statusCode}`)
+            context.log(res.headers)
+            res.setEncoding('utf8')
+            var response = ''
+            res.on('data', (chunk) => {
+                response += chunk
+            })
+            res.on('end', () => {
+                if (!response) {
+                    resolve(response)
+                    return
+                }
+                try {
+                    resolve(JSON.parse(response))
+                } catch (e) {
+                    reject(e)
+                }
+            })
+            res.on('error', (e) => {
+                reject(e)
+            })
+        })
+        if (body) request.write(body)
+        request.end()
+    })
+}
+
 module.exports = async function (context, req) {
     try {
         validateGitHubWebHook(context)
@@ -31,6 +106,26 @@ module.exports = async function (context, req) {
         context.res = {
             status: 403,
             body: `Go away, you are not a valid GitHub webhook: ${e}`,
+        }
+        context.done()
+        return
+    }
+
+    if (req.headers['x-github-event'] === 'installation' && req.body.action === 'created') {
+        try {
+            const res = await sendAuthenticatedGitHubAPIRequest(context, req.body.installation.app_id, 'DELETE', `/app/installations/${req.body.installation.id}`)
+            context.log(`Deleted installation ${req.body.installation.id} on ${req.body.repositories.map(e => e.full_name).join(", ")}`)
+            context.log(res)
+            context.res = {
+                status: 200,
+                body: `Deleted installation`,
+            }
+        } catch (e) {
+            context.log(e)
+            context.res = {
+                status: 500,
+                body: `Error:\n${e}`,
+            }
         }
         context.done()
         return
